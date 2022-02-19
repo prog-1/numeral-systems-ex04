a1ccf04696e9eecd1ae2a420b855180e8652f947
Alina Aniscenko <alina065b@gmail.com>
2022-02-19 19:32:45 +0200

    bug fix

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import "fmt"
 
-func addZeros(a string, i int) (res string) {
-	for ; i > 0; i-- {
-		a = "0" + a
-	}
-	return a
-}
-
 func LongMul(a, b string) (res string) {
 	if len(a) > len(b) {
 		a, b = b, a
@@ -19,18 +12,48 @@ func LongMul(a, b string) (res string) {
 		toSum[i] = make([]byte, len(b)+1)
 		for i1 := range b {
 			sum := carry + (a[len(a)-1-i]-'0')*(b[len(b)-1-i1]-'0')
-			toSum[i][len(toSum[i])-1-i1], carry = sum%10, sum/10
+			toSum[i][len(toSum[i])-1-i1], carry = sum%10+'0', sum/10
 		}
 		if carry > 0 {
-			toSum[i][0] = carry
+			toSum[i][0] = carry + '0'
 		} else {
 			toSum[i] = toSum[i][1:]
 		}
 	}
-	fmt.Println(toSum)
+	for i := range toSum {
+		for j := i; j > 0; j-- {
+			toSum[i] = append(toSum[i], '0')
+		}
+	}
+	for _, v := range toSum {
+		res = LongAdd(res, string(v))
+	}
 	return
 }
 
+func addZeros(a string, i int) string {
+	for ; i > 0; i-- {
+		a = "0" + a
+	}
+	return a
+}
+
+func LongAdd(a, b string) (res string) {
+	if len(a) > len(b) {
+		a, b = b, a
+	}
+	var carry byte
+	a = addZeros(a, len(b)-len(a))
+	for i := len(a) - 1; i >= 0; i-- {
+		num := (a[i] - '0') + (b[i] - '0') + carry
+		carry = num / 10
+		res = string(num%10+'0') + res
+	}
+	if carry > 0 {
+		res = "1" + res
+	}
+	return res
+}
 
 func main() {
 	fmt.Println("Program implements arithmetics multiplication - accepts two decimal numbers encoded as strings.")
